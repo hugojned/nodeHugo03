30869ad94b6135796a078da15b3a69aa70408c8b
Hugo Ned <jugoxnet667@gmail.com>
2024-04-29 11:23:35 -0600

    eee

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const base = 5;
+
+
+console.clear();
+
+console.log('=======================')
+console.log('Tabla del:', base)
+console.log('=======================')
+
+for ( let i = 1; i <= 10; i++ ){
+    console.log(`${ base } x ${ i } = ${ base * i }`)
+}
